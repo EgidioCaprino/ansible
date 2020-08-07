f823988e4c5faf8db0c26f2a395f6be1a617e7c3
Egidio Caprino <egidio.caprino@gmail.com>
2020-08-07 11:49:30 +0200

    chore(linode): remove website and drone from Linode server

--- roles/borg/files/backup_docker_volumes.sh
+++ roles/borg/files/backup_docker_volumes.sh
@@ -5,23 +5,18 @@ systemctl restart keep_connection_alive.service
 repository="/mnt/borg_repository"
 mountDir="/mnt/egidiocaprino.com"
 
-sshfs root@egidiocaprino.com:/ "${mountDir}"
+sshfs root@nextcloud.egidiocaprino.com:/ "${mountDir}"
 
-ssh -l root egidiocaprino.com "docker stop drone_server"
-timeout --kill-after 1m 1h borg create "${repository}"::var_lib_drone-{now:%Y-%m-%d} "${mountDir}"/var/lib/drone
-ssh -l root egidiocaprino.com "docker start drone_server"
-
-ssh -l root egidiocaprino.com "docker stop nextcloud_app_1"
-ssh -l root egidiocaprino.com "docker stop nextcloud_db_1"
+ssh -l root nextcloud.egidiocaprino.com "docker stop nextcloud_app_1"
+ssh -l root nextcloud.egidiocaprino.com "docker stop nextcloud_db_1"
 
 timeout --kill-after 1m 3h borg create "${repository}"::var_lib_docker_volumes_nextcloud_db-{now:%Y-%m-%d} "${mountDir}"/var/lib/docker/volumes/nextcloud_db
 timeout --kill-after 1m 3h borg create "${repository}"::var_lib_docker_volumes_nextcloud_nextcloud-{now:%Y-%m-%d} "${mountDir}"/var/lib/docker/volumes/nextcloud_nextcloud
 
-ssh -l root egidiocaprino.com "docker start nextcloud_db_1"
-ssh -l root egidiocaprino.com "docker start nextcloud_app_1"
+ssh -l root nextcloud.egidiocaprino.com "docker start nextcloud_db_1"
+ssh -l root nextcloud.egidiocaprino.com "docker start nextcloud_app_1"
 
 umount "${mountDir}"
 
-timeout --kill-after 1m 1h borg prune --prefix "var_lib_drone" --keep-last 30 "${repository}"
 timeout --kill-after 1m 1h borg prune --prefix "var_lib_docker_volumes_nextcloud_db" --keep-last 30 "${repository}"
 timeout --kill-after 1m 1h borg prune --prefix "var_lib_docker_volumes_nextcloud_nextcloud" --keep-last 30 "${repository}"
